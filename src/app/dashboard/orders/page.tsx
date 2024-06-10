bc529f16ea98d29f3d646c638e8c981026fa223f
farhankhanphp <farhankhan.php8@gmail.com>
2024-06-10 22:47:21 +0500

    order form

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -19,8 +19,8 @@ import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
-import OrderListing from "@/section/order-listing/page";
-import Header from "@/section/header/page";
+import OrderListing from "@/components/order-listing/page";
+import Header from "@/components/header/page";
 
 const drawerWidth = 240;
 
@@ -133,32 +133,7 @@ export default function MiniDrawer() {
     </DrawerHeader>
     <Divider />
     <List>
-     {["Dashboard", "Orders"].map((text, index) => (
-      <ListItem key={text} disablePadding sx={{ display: "block" }}>
-       <ListItemButton
-        sx={{
-         minHeight: 48,
-         justifyContent: open ? "initial" : "center",
-         px: 2.5,
-        }}
-       >
-        <ListItemIcon
-         sx={{
-          minWidth: 0,
-          mr: open ? 3 : "auto",
-          justifyContent: "center",
-         }}
-        >
-         {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-        </ListItemIcon>
-        <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-       </ListItemButton>
-      </ListItem>
-     ))}
-    </List>
-    <Divider />
-    <List>
-     {["user"].map((text, index) => (
+     {["Orders"].map((text, index) => (
       <ListItem key={text} disablePadding sx={{ display: "block" }}>
        <ListItemButton
         sx={{
