36e9d4cbe55173a17a1028f7d0379055d6bbea02
farhankhanphp <farhankhan.php8@gmail.com>
2024-06-13 18:38:34 +0500

    Create Order Form Components

--- src/components/forms/order-form.tsx
+++ src/components/forms/order-form.tsx
@@ -1,115 +1,240 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { Typography, Grid, Chip } from '@mui/material';
 import Divider from '@mui/material/Divider';
 import BasicTextFields from '@/components/basic-text-fields';
-import MultiSelect from '@/components/multi-select';
-import DynamicBox from '@/components/cards/basket-card';
+import SingleSelect from '../single-select';
+import MultipleSelectCheckmarks from '../multiple-select-checkmarks';
+
 
 type OrderCardProps = {
- orderTitle?: string;
- employeeName?: string;
- customerPrice?: string;
- remainingAmount?: string;
- mode: "add" | "edit";
+    orderTitle?: string;
+    employeeName?: string;
+    customerPrice?: string;
+    remainingAmount?: string;
+    mode: "add" | "edit";
 };
 
-const employeeOptions = [
- { label: 'Mark', color: '#ff8a80' },
- { label: 'John', color: '#ff80ab' },
- { label: 'Jane', color: '#b39ddb' },
+const names = [
+    'Oliver Hansen',
+    'Van Henry',
+    'April Tucker',
 ];
-const dynamicData = {
- title: "basket 1",
- price: 10.99,
- tags: ["tag1", "tag2", "tag3"],
-};
+
+const records = [
+    { name: 'Oliver Hansen', price: '$10' },
+    { name: 'Van Henry', price: '$20' },
+    { name: 'April Tucker', price: '$15' },
+    { name: 'Ralph Hubbard', price: '$25' },
+];
+const packing = [
+    { name: 'Cotton', price: '$10' },
+    { name: 'Polyester', price: '$20' },
+    { name: 'Wool', price: '$15' },
+]
+const freshFlower = [
+    { name: 'rose', price: '$10' },
+    { name: 'tulip', price: '$20' },
+    { name: 'sunflower', price: '$15' },
+]
 
 const OrderForm: React.FC<OrderCardProps> = ({
- orderTitle,
- employeeName,
- customerPrice,
- remainingAmount,
- mode,
+    orderTitle,
+    customerPrice,
+    remainingAmount,
+    mode,
 }) => {
- return (
-  <>
-   <Grid container gap={2} flexWrap="nowrap">
-    <Grid container item sm={8} gap={2}>
-     <Grid item xs={12} container alignItems="center">
-      <Grid item xs={12} sm={3}>
-       <Typography variant="h6">Order Title</Typography>
-      </Grid>
-      <Grid item xs={12} sm={9}>
-       <BasicTextFields size="small" defaultValue={mode === "edit" ? orderTitle : ""} fullWidth />
-      </Grid>
-     </Grid>
-     <Grid item xs={12} container alignItems="center">
-      <Grid item xs={12} sm={3}>
-       <Typography variant="h6">Employee Name</Typography>
-      </Grid>
-      <Grid item xs={12} sm={9}>
-       <MultiSelect options={employeeOptions} label='' />
-      </Grid>
-     </Grid>
-     <Grid item xs={12} container alignItems="center">
-      <Grid item xs={12} sm={3}>
-       <Typography variant="h6">Customer Price</Typography>
-      </Grid>
-      <Grid item xs={12} sm={9}>
-       <BasicTextFields size="small" defaultValue={mode === "edit" ? customerPrice : ""} fullWidth />
-      </Grid>
-     </Grid>
-     <Grid item xs={12} container alignItems="center">
-      <Grid item xs={12} sm={3}>
-       <Typography variant="h6">Remaining</Typography>
-      </Grid>
-      <Grid item xs={12} sm={9}>
-       <BasicTextFields size="small" defaultValue={mode === "edit" ? remainingAmount : ""} fullWidth />
-      </Grid>
-     </Grid>
-     <Grid item xs={12}>
-      <Divider>
-       <Chip label="Basket Details" size="small" />
-      </Divider>
-     </Grid>
-     <Grid item xs={12} container alignItems="center">
-      <Grid item xs={12} sm={3}>
-       <Typography variant="h6">Remaining</Typography>
-      </Grid>
-      <Grid item xs={12} sm={9}>
-       <DynamicBox
-        title={dynamicData.title}
-        price={dynamicData.price}
-        tags={dynamicData.tags}
-        onClose={() => {
-         // Handle close event
-        }}
-       />
-      </Grid>
-     </Grid>
-    </Grid>
+    const [selectedName, setSelectedName] = useState<string>("");
 
-    {/* Sidebar */}
-    <Grid item>
-     <Divider orientation="vertical" />
-    </Grid>
 
-    <Grid item sm={4}>
-     <DynamicBox
-      title={dynamicData.title}
-      price={dynamicData.price}
-      tags={dynamicData.tags}
-      onClose={() => {
-       // Handle close event
-      }}
-     />
-    </Grid>
+    const handleNameChange = (value: string) => {
+        setSelectedName(value);
+    };
 
-   </Grid>
-  </>
- );
+    return (
+        <>
+            <Grid container gap={2} flexWrap="nowrap">
+                <Grid container item sm={5} gap={4}>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Order Title</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields label="Order Name" defaultValue={mode === "edit" ? orderTitle : ""} fullWidth />
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Employee Name</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <SingleSelect
+                                options={names}
+                                label="Select the Name"
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Customer Price</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields label="Total Budget" defaultValue={mode === "edit" ? customerPrice : ""} fullWidth />
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Remaining</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields label="Remaining Budget" defaultValue={mode === "edit" ? remainingAmount : ""} fullWidth />
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Divider>
+                            <Chip label="Records Details" size="small" />
+                        </Divider>
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Hard Goods</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <MultipleSelectCheckmarks records={records} />
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Hard Goods Price</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <Typography variant="body1" color="text.primary">
+                                $20.45
+                            </Typography>
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Greens</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields 
+                                InputProps={{
+                                    readOnly: true, 
+                                    notched: true,
+                                }}
+                                InputLabelProps ={{ shrink: true }} label="Greens Price" defaultValue='20.45' fullWidth />
+                        </Grid>                    
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Packaging</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <MultipleSelectCheckmarks records={packing} />
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Packing Price</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields 
+                                InputProps={{
+                                    readOnly: true, 
+                                    notched: true,
+                                }}
+                                InputLabelProps ={{ shrink: true }} label="Packing Price" defaultValue='15.0' fullWidth />
+                        </Grid>                    
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Fresh Flower qty</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <MultipleSelectCheckmarks records={freshFlower} />
+                        </Grid>
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Remaining</Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields 
+                                InputProps={{
+                                    readOnly: true, 
+                                    notched: true,
+                                }}
+                                InputLabelProps ={{ shrink: true }} label="Remaining" defaultValue='5.00' fullWidth />
+                        </Grid>                    
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Quantity </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields 
+                                InputProps={{
+                                    readOnly: true, 
+                                    notched: true,
+                                }}
+                                InputLabelProps ={{ shrink: true }} label="From Fresh FLowers Quantity" defaultValue='5.00' fullWidth />
+                        </Grid>                    
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Price </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields 
+                                InputProps={{
+                                    readOnly: true, 
+                                    notched: true,
+                                }}
+                                InputLabelProps ={{ shrink: true }} label="(from Fresh Flowers) (from Fresh FLowers Quantity)" defaultValue='5.00' fullWidth />
+                        </Grid>                    
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Flower Price </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields 
+                                InputProps={{
+                                    readOnly: true, 
+                                    notched: true,
+                                }}
+                                InputLabelProps ={{ shrink: true }} label="Flower Price" defaultValue='5.00' fullWidth />
+                        </Grid>                    
+                    </Grid>
+                    <Grid item xs={12} container alignItems="center">
+                        <Grid item xs={12} sm={3}>
+                            <Typography variant="h6">Wastage </Typography>
+                        </Grid>
+                        <Grid item xs={12} sm={9}>
+                            <BasicTextFields 
+                                InputProps={{
+                                    readOnly: true, 
+                                    notched: true,
+                                }}
+                                InputLabelProps ={{ shrink: true }} label="Wastage Ammount" defaultValue='8.00' fullWidth />
+                        </Grid>                    
+                    </Grid>
+                </Grid>
+
+                {/* Sidebar */}
+                <Grid item >
+                    <Divider orientation="vertical" />
+                </Grid>
+
+                <Grid item sm={7}>
+
+                </Grid>
+
+            </Grid>
+        </>
+    );
 };
 
 export default OrderForm;
